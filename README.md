7f6f97c6bc0ccd35a139ee443662ac9f3086ecfb
lzhnb <eezhihaoliang@mail.scut.edu.cn>
2022-12-03 16:04:49 +0800

    feat: add the superpixel extraction script and update README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # torch_mvs
+
+## Installing
+```sh
+python setup.py develop
+```
+
+## Preprocessing
+```sh
+python scripts/colmap2mvs.py --dense_folder $DATA_DIR/dense/ --save_folder $RESULT_ROOT
+```
+
+## Extract superpixels
+```sh
+python scripts/extract_superpixel.py --img_dir $DATA_DIR/images --save_dir $SUPERPIXEL_DIR
+```
+
+## Running
+```sh
+python -m tmvs.launch -rf $RESULT_ROOT # --suffix $MVS_SUFFIX)(optional)
+```
--- tmvs/launch.py
+++ tmvs/launch.py
@@ -18,6 +18,12 @@ if __name__ == "__main__":
         help="result_folder to store the dense results of COLMAP.",
     )
     parser.add_argument(
+        "--suffix",
+        default="default",
+        type=str,
+        help="suffix directory to store the mvs results.",
+    )
+    parser.add_argument(
         "--geom_iterations",
         "-gi",
         type=int,
@@ -42,7 +48,7 @@ if __name__ == "__main__":
     result_folder = args.result_folder
     problems = _C.generate_sample_list(os.path.join(result_folder, "pair.txt"))
 
-    os.makedirs(os.path.join(result_folder, "ACMP"), exist_ok=True)
+    os.makedirs(os.path.join(result_folder, args.suffix), exist_ok=True)
 
     num_images = len(problems)
     print(f"There are {num_images} problems needed to be processed!")
@@ -55,7 +61,7 @@ if __name__ == "__main__":
         depth_map, normal_map, cost_map = _C.process_problem(
             result_folder, problems[i], False, args.planar_prior, False, pmmvs
         )
-        save_folder = os.path.join(result_folder, "ACMP", f"{i:04}")
+        save_folder = os.path.join(result_folder, args.suffix, f"{i:04}")
         os.makedirs(save_folder, exist_ok=True)
         np.save(os.path.join(save_folder, "depths.npy"), depth_map)
         np.save(os.path.join(save_folder, "normals.npy"), normal_map)
@@ -67,7 +73,7 @@ if __name__ == "__main__":
         all_normals = []
         all_costs = []
         for i in trange(num_images, desc="loading for geometric consistent"):
-            save_folder = os.path.join(result_folder, "ACMP", f"{i:04}")
+            save_folder = os.path.join(result_folder, args.suffix, f"{i:04}")
             depth_suffix = "depths_geom.npy" if multi_geometry else "depths.npy"
             all_depths.append(np.load(os.path.join(os.path.join(save_folder, depth_suffix))))
             all_normals.append(np.load(os.path.join(os.path.join(save_folder, "normals.npy"))))
@@ -84,7 +90,7 @@ if __name__ == "__main__":
             depth_map, normal_map, cost_map = _C.process_problem(
                 result_folder, problems[i], True, False, multi_geometry, pmmvs
             )
-            save_folder = os.path.join(result_folder, "ACMP", f"{i:04}")
+            save_folder = os.path.join(result_folder, args.suffix, f"{i:04}")
             os.makedirs(save_folder, exist_ok=True)
             np.save(os.path.join(save_folder, "depths_geom.npy"), depth_map)
             np.save(os.path.join(save_folder, "normals.npy"), normal_map)
